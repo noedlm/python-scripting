745803ffb2212d10c7ff754c42c5165373cb1fe2
Noe De La Mora <noe.delamora@univar.com>
2019-01-29 14:01:06 -0600

    more doodles

--- python_scripts/doodles.py
+++ python_scripts/doodles.py
@@ -12,3 +12,15 @@ filled_dict = {"one": 1, "two": 2, "three": 3}
 it = filled_dict.keys()
 print(it) # dict_keys(['one', 'two', 'three'])
 print(list(filled_dict.keys())) # ['one', 'two', 'three']
+
+arr = [1,2,3,4,5,6,3,2,5,6,3,2,4,5,3,2,5,5,3,2,4,4]
+print(dict.fromkeys(reversed(arr)))
+print(list(reversed(list(dict.fromkeys(reversed(arr))))))
+alist = []
+print(reversed(alist))
+for i in reversed(alist):
+    print(i)
+
+print(alist)
+re = 'abc'
+print(re[-1])
