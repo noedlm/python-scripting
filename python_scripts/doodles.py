6fe90d3ebaf0c5b17147e2d673c5f7031cc602da
Noe De La Mora <noedelamora@gmail.com>
2019-01-29 19:13:00 -0600

    doodles

--- python_scripts/doodles.py
+++ python_scripts/doodles.py
@@ -13,14 +13,46 @@ it = filled_dict.keys()
 print(it) # dict_keys(['one', 'two', 'three'])
 print(list(filled_dict.keys())) # ['one', 'two', 'three']
 
-arr = [1,2,3,4,5,6,3,2,5,6,3,2,4,5,3,2,5,5,3,2,4,4]
-print(dict.fromkeys(reversed(arr)))
-print(list(reversed(list(dict.fromkeys(reversed(arr))))))
-alist = []
-print(reversed(alist))
-for i in reversed(alist):
-    print(i)
+array_sides = [1,2,3,4,3,2,1]
 
-print(alist)
-re = 'abc'
-print(re[-1])
+def sides(arr):
+    for i in range(0, len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
+            return i
+
+    return -1
+
+print(sides(array_sides))
+
+'''
+Description: Next biggest number with the same digits.
+Ex: 12 => 21
+Ex: 13215664 => 13216456
+Ex: 13215964 => 13216459
+
+TODO: need to check the number that's 
+ '''
+
+
+nbn = list(str(13215964))
+index = -1
+
+for i in range(len(nbn)-1, 0, -1):
+    if nbn[i] > nbn[i-1]:
+        nbn = nbn[:i-1] + sorted(nbn[i-1:len(nbn)], reverse=True)
+        nbn = nbn[:i] + sorted(nbn[i:len(nbn)])
+        index = i
+        break
+
+print(''.join(nbn))
+
+
+
+nbn[index] = nbn[index+1]
+
+print(nbn)
+
+print(index)
+print(sub_string)
+
+print(''.join(sorted(list(nbn[i:]))))
